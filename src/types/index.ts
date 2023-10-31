c356db8c0b9d1677c033041833010ed2f6d42039
Ragura <ragura@gmail.com>
2023-10-31 21:48:26 +0100

    feat: export more types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,11 @@
-export type { Game } from './games'
+export type * from './games'
+export type { Collection, CollectionItem } from './collection'
+export type {
+  Poll,
+  LanguageDependence,
+  LanguageDependenceResult,
+  SuggestedPlayerAge,
+  SuggestedPlayerAgeResult,
+  SuggestedPlayers,
+  SuggestedPlayersResult
+} from './poll'
